2c92ef17bd54dc82ddb1ad25ceef54d95cf56055
Patrick <patrickburns2557@gmail.com>
2022-04-14 20:11:58 -0500

    Added MovieGrid Method

--- src/View/MovieGrid.java
+++ src/View/MovieGrid.java
@@ -13,17 +13,19 @@ public class MovieGrid extends JPanel
 {
     public static final int MOVIE_WIDTH = 220;
     public static final int MOVIE_HEIGHT = 330;
+    ArrayList<JButton> buttonList;
+    ArrayList<Movie> movieList;
 
     public MovieGrid(Collection list) //REPLACE WITH MOVIELIST LATER
     {
-        ArrayList<Movie> movieList = list.getMovies();
+        movieList = list.getMovies();
         this.setLayout(new WrapLayout(FlowLayout.LEADING, 15, 15)); // Wrap Layout extends Flowlayout and just
         // properly wraps to the next line when runs out of horizontal space
         // Regular FlowLayout doesn't wrap to the next line when a JScrollPane is added to it
 
 
         //Create Buttons for each movie listed in the grid
-        ArrayList<JButton> buttonList = new ArrayList<JButton>();
+        buttonList = new ArrayList<JButton>();
         for(Movie movie : movieList)
         {
             //Image img = new ImageIcon(CreatePoster.getFromURL(movie.getposter(), movie.gettitle(), movie.getyear()).getImage(); //create image from the poster link
@@ -34,21 +36,27 @@ public class MovieGrid extends JPanel
         //add each button to the MovieGrid JPanel
         for(int i = 0; i < movieList.size(); i++)
         {
-            final int final_i = i;
-            //resize the poster to fit on the button for it
-            buttonList.get(i).setPreferredSize(new Dimension(MOVIE_WIDTH,MOVIE_HEIGHT));
-            buttonList.get(i).addActionListener(new
-                                                        ActionListener()
-                                                        {
-                                                            @Override
-                                                            public void actionPerformed(ActionEvent e)
-                                                            {
-                                                                System.out.println(movieList.get(final_i).gettitle() + " button pressed");
-                                                                MainWindow view = MainWindow.getInstance();
-                                                                view.ShowMovie(movieList.get(final_i));
-                                                            }
-                                                        });
-            this.add(buttonList.get(i));
+            SelectMovie(i);
         }
+    }
+
+
+    private void SelectMovie(int i)
+    {
+        final int final_i = i;
+        //resize the poster to fit on the button for it
+        buttonList.get(i).setPreferredSize(new Dimension(MOVIE_WIDTH,MOVIE_HEIGHT));
+        buttonList.get(i).addActionListener(new
+                                                    ActionListener()
+                                                    {
+                                                        @Override
+                                                        public void actionPerformed(ActionEvent e)
+                                                        {
+                                                            System.out.println(movieList.get(final_i).gettitle() + " button pressed");
+                                                            MainWindow view = MainWindow.getInstance();
+                                                            view.ShowMovie(movieList.get(final_i));
+                                                        }
+                                                    });
+        this.add(buttonList.get(i));
     }
 }
