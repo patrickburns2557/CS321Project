2c1dadc01f063b9427de39e35c6f8cc7b7a61fcb
Patrick <patrickburns2557@gmail.com>
2022-04-21 13:11:59 -0500

    Changed Theme to "Nimbus"

--- src/RunProgram.java
+++ src/RunProgram.java
@@ -1,9 +1,21 @@
 import View.MainWindow;
 
+import javax.swing.*;
+
 public class RunProgram
 {
     public static void main(String[] args)
     {
+        try
+        {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        }
+        catch(Exception ex)
+        {
+            //Do nothing because if it fails, will just fallback to default look
+        }
+
+
         MainWindow view = MainWindow.getInstance();
         view.setVisible(true);
     }
