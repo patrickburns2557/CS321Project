c75d37af4b16f31e6062edb65ee0cbeaefea86c1
Cjack-20 <csj0017@uah.edu>
2022-04-21 20:26:49 -0500

    JavaDocs

--- src/Model/Collection.java
+++ src/Model/Collection.java
@@ -7,16 +7,29 @@ public class Collection implements Cloneable {
     private String name;
     private ArrayList<Movie> movies;
 
+    /**
+     * Public constructor that holds the variable name and Array List
+     * @param name
+     */
     public Collection(String name) {
         this.name = name;
         this.movies = new ArrayList<>();
     }
+    /**
+     * Public constructor that holds the variable name and Array List object movie
+     * @param name
+     */
 
     public Collection(String name, ArrayList<Movie> movies)
     {
         this.name = name;
         this.movies = movies;
     }
+
+    /**
+     * Public Clone method used to make the Collection class Cloneable
+     * @return
+     */
 
     public Collection clone()
     {
@@ -29,27 +42,63 @@ public class Collection implements Cloneable {
         }
     }
 
+    /**
+     * Gets the name of the collection
+     * @return
+     */
+
     public String getName() {
+
         return name;
     }
 
+    /**
+     * Sets the name of the collection
+     * @return
+     */
+
     public void setName(String name) {
+
         this.name = name;
     }
 
+    /**
+     * Gets the movie from the Arrary List
+     * @return
+     */
+
     public ArrayList<Movie> getMovies() {
+
         return movies;
     }
 
+    /**
+     * Adds the movie to the Arrary List
+     * @return
+     */
+
     public void addMovie(Movie movie){
+
         movies.add(movie);
     }
 
+    /**
+     * Removes the movie from the Arrary List
+     * @return
+     */
+
     public void removeMovie(Movie movie){
+
         movies.remove(movie);
     }
 
+    /**
+     * Deletes the Collection
+     * @return
+     */
+
     public void deleteCollection(){
+
         movies.clear();
     }
 }
